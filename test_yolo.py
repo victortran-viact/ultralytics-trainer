169a9daf0f92a25fc9a229ddccb7f862bed4d44c
dxv2k <vu.dinh@viact.ai>
2023-08-24 12:25:01 +0700

    feat(test stage): add evaluate for YOLOv5/YOLOv8

--- test_yolo.py
+++ test_yolo.py
@@ -2,8 +2,10 @@ import os
 from ultralytics import YOLO 
 from clearml import Dataset
 
+
 from utils.clearml_utils import download_model
-   
+
+
 
 def get_dataset_from_storage(dataset_id: str) -> str:
     """
@@ -47,15 +49,26 @@ def get_dataset_from_storage(dataset_id: str) -> str:
 
 
 
-def finetune_yolo(
-        dataset_id: str,
-        model_id: str,
-        batch_size: int = 16,
-        imgsz: int = 640,
-        epochs: int = 10,
-) -> None:
-
-    # yaml_filepath = get_dataset_zip_from_storage(dataset_id=dataset_id)
+def test_yolo(
+    dataset_id: str,
+    model_id: str,
+) -> None: 
+    """
+    Sample output: ultralytics.utils.metrics.DetMetrics object with attributes:
+    ```
+        ap_class_index: array([0])
+        box: ultralytics.utils.metrics.Metric object
+        confusion_matrix: <ultralytics.utils.metrics.ConfusionMatrix object at 0x7fd3a588fac0>
+        fitness: 0.12331301833487544
+        keys: ['metrics/precision(B)', 'metrics/recall(B)', 'metrics/mAP50(B)', 'metrics/mAP50-95(B)']
+        maps: array([    0.11055,     0.11055])
+        names: {0: 'canvas', 1: 'person'}
+        plot: True
+        results_dict: {'metrics/precision(B)': 0.05601317957166392, 'metrics/recall(B)': 1.0, 'metrics/mAP50(B)': 0.23813998138422993, 'metrics/mAP50-95(B)': 0.11055446688494716, 'fitness': 0.12331301833487544}
+        save_dir: PosixPath('runs/detect/val2')
+        speed: {'preprocess': 0.11606216430664062, 'inference': 8.380484580993652, 'loss': 0.00152587890625, 'postprocess': 1.4193296432495117}
+    ```
+    """
     yaml_filepath = get_dataset_from_storage(dataset_id=dataset_id)
 
     print(f"Dataset is stored at {yaml_filepath}")
@@ -63,13 +76,14 @@ def finetune_yolo(
 
     model_path = download_model(model_id)
     model = YOLO(model_path)
-    model.train(
+    
+    metrics = model.val(
         data=yaml_filepath,
-        imgsz=imgsz,
-        epochs=epochs,
-        cache='ram',
-        batch=batch_size
     )
+
+ 
+    return metrics 
+
 
 
 if __name__ == "__main__":
@@ -82,20 +96,9 @@ if __name__ == "__main__":
         "--model_id", help="Model from ClearML Registry", type=str, default=None
     )
 
-    args.add_argument(
-        "--batch_size", default=16, help="Batch size"
-    )
-    args.add_argument(
-        "--imgsz", default=640, help="Image size"
-    )
-    args.add_argument(
-        "--epochs", default=10, help="Epochs", type=int
-    )
     args = args.parse_args()
     
-    finetune_yolo(
+    test_yolo(
         dataset_id=args.dataset_id,
         model_id=args.model_id,
-        batch_size=args.batch_size,
-        imgsz=args.imgsz,
-        epochs=args.epochs)
+    )
